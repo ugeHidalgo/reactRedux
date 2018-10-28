a1312dcd03455b5329649f921a10fbfa81f991af
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-10-28 17:57:14 +0100

    Initial test implemented using ES6

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 import expect from 'expect';
 
-describe('',function(){
-    it('',function(){
+describe('Initial tests',() => {
+    it('Should launch test properly', () => {
         expect(true).toEqual(true);
     });
 });
