06fdf95f7ed4ba360d3137a49ddf9b39eef42def
ugeHidalgo <ugeHidalgo@hotmail.com>
2018-11-05 18:57:50 +0100

    Added a layout template page.

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+
+class Layout extends React.Component {
+    render () {
+        return (
+            <div className="container-fluid">
+                <p>Header here...</p>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+Layout.propTypes = {
+    children: PropTypes.object.isRequired
+};
+
+export default Layout;
+
